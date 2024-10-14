22ab69ddf984fbd8fa565f3ef94ffe7ddf72ce86
okrecommendation9394 <qetigogia50@gmail.com>
2024-10-14 12:58:49 +0400

    oop

--- src/dev/omedia/Main.java
+++ src/dev/omedia/Main.java
@@ -1,8 +1,18 @@
 package dev.omedia;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SimpleCalculator sc = new SimpleCalculator();
+        sc.setFirstNumber(5.8);
+        sc.setSecondNumber(4.4);
+        System.out.println(sc.getDivisionResult());
+        Carpet carpet = new Carpet(8);
+        Floor floor = new Floor(10,12);
+        Calculator calc = new Calculator(floor, carpet);
+        System.out.println(calc.getTotalCost());
     }
 }
